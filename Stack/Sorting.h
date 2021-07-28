1aa5491b071e639ce7c63846283f088afc9f7c11
adr1an <adrn.cederberg123@gmail.com>
2021-07-28 17:17:54 -0600

    Started on the smart sort method and made other small improvements.

--- Stack/Sorting.h
+++ Stack/Sorting.h
@@ -4,7 +4,7 @@
 # define STACKSORT
 
 // Sort in-place the memory array.
-void arraySort(Stack* S){
+void naiveSort(Stack* S){
 	
 	int temp;
 
@@ -25,4 +25,86 @@ void arraySort(Stack* S){
 
 }
 
+// --------------------------------------------------------------
+
+// Not necessary, but it useful. 
+// Would be coold to sort using more than three. Could realloc.
+struct Game{
+
+	// Things well need to remember for the game.
+	// Keep tract of empty columns, size, capacity, etc
+	bool hasmt[3];
+	int size;
+	int capacity;
+	int last; //index of the last moved column
+	Stack* one;
+	Stack* two;
+	Stack* game[3];
+
+};
+
+typedef struct Game Game;
+
+// Make a new Game struct
+Game* newGame(Stack* S){
+
+	Game* G = (Game *)malloc( sizeof(Game) );
+	G -> last = 0;
+	G -> hasmt[0], G -> hasmt[1], G -> hasmt[2] = true, false, false;
+	G -> size = 3;
+	G -> capacity = S-> capacity;
+	G -> game[0] = S;
+	G -> game[1] = newStack( S->capacity );
+	G -> game[2] = newStack( S->capacity );
+		
+//	printf("size = %i\n", G -> size);
+	
+	return G;
+
+}
+
+void show(Game* G){
+	for(int k = 0; k < G->size; k++){
+		show( G -> game[k], 0 );
+	}
+}
+// If a header arguement is given at all prints header. If true, prints headers for stacks
+void show(Game* G, bool header){	
+	printf("size = %i,\tlast = %p,\tcapacity = %i.\n", G->size, G->last, G->capacity);
+	for(int k = 0; k < G->size; k++){
+		show( G -> game[k], header );
+	}	
+}
+void mem(Game* G){ for(int k = 0; k < G->size; k++){ mem( G -> game[k] ); }
+}
+int empty(Game* G){
+	// return index of empty if exists else -1.
+	for (int k = 0; k < G -> size; k++ ){	
+		if ( G -> hasmt[k] ){ return k; }
+	}
+	return -1;
+}
+int fillWith(Game* G, int column){
+	// column is the output of empty
+	for(int k = 0; k < G -> size; k++){
+		if ( k != column & k != G -> last ){
+			return k;
+			break;
+		}
+	}
+	return -1;
+}
+void fill(Game* G, int column){
+	// Rule two as in my notes
+	fillWith( G, column );
+}	
+void move(Game* G, Stack* firstColumn, Stack* secondColumn){
+	// Rule one as in my notes. `turn` will hold on to distances and determine firstcolumn and second column.
+			
+	return;
+}
+void turn(Game* G){
+
+	return;
+}
 # endif
--- Stack/SortingMain.c
+++ Stack/SortingMain.c
@@ -1,6 +1,6 @@
 # include "Sorting.h"
 
-int test_arraySort(){
+int test_naiveSort(){
 
 	Stack* S = newStack(5);
 	push(S, 2);
@@ -11,9 +11,32 @@ int test_arraySort(){
 	show(S, 1);
 	
 	// Sort the underlying memory
-	arraySort(S);
+	naiveSort(S);
 	show(S, 1);
 	
+	return 0;
+
+}
+
+void test_smartSort(){
+	
+	Stack* S = newStack(6);
+	push(S, 4); push(S, 1); push(S, 5); push(S, 3); push(S, 6); push(S, 2);
+	//show(S, 1);
+
+	Game* G = newGame(S);
+	show(G,1);
+	push( G -> game[1], 4);
+
+	printf( "\nempty = %i", fillWith(G, 2) ) ;
+//	show(G -> game[0], 0);
+//	show(G -> game[1], 0);
+//	show(G -> game[2], 0);
+
+}
+int main(){
+
+	test_smartSort();
 	return 0;
 
 }
